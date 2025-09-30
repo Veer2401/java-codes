1c8cf5aadc14a116f7727c6a1fc665dff3b11e10
Veer Harischandrakar <1012411025@despu.edu.in>
2025-09-30 18:25:25 +0530

    Refactor FileIO to use new file path and improve error handling

--- FileIO.java
+++ FileIO.java
@@ -1,59 +1,45 @@
+package study;
 
-//java assignment 8
-
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.FileReader;
+import java.io.*;
 import java.util.Scanner;
-import java.io.FileNotFoundException;
 
 public class FileIO{
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        String f = "/Users/veer/desktop/assignment8.txt";
-        
-        try{
-            File file = new File(f);
-            if(file.exists()){
-                System.out.println("Chats: ");
-                try(BufferedReader r = new BufferedReader(new FileReader(file))){
-                    String line;
-                    while((line = r.readLine()) != null){
-                        System.out.println(line);
-                    }
-                }catch(FileNotFoundException e){
-                    System.out.println(e);
-                }
-            } else{
-                System.out.println("No chat was found. Start a new chat");
-            }
-
-        
-
-        try(BufferedWriter w = new BufferedWriter(new FileWriter(f,true))){
-            System.out.println("Type your message. Type '0' to stop.\n");
-
-            while(true){
-                System.out.println("Start..");
-                String input = sc.nextLine();
-
-                if(input.equalsIgnoreCase("0")){
-                    break;
-                }
-
-                w.write(input);
-                w.newLine();
-                w.flush();
-        } 
-    }
-    } catch (IOException e){
-
-        } finally{
-            sc.close();
-        }
-    }  
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String filePath = "C:\\Users\\Matrix It Solutions\\Desktop\\text.txt";
+		
+		try {
+			try(BufferedReader r = new BufferedReader(new FileReader(filePath))){
+				String line;
+				while((line = r.readLine()) != null) {
+					System.out.println(line);
+				}
+			}catch(FileNotFoundException e) {
+				System.out.println("Error " + e.getMessage());
+			}
+			
+			
+			try(BufferedWriter w = new BufferedWriter(new FileWriter(filePath,true))){
+				System.out.println("Type your message.. Type '0' to close.");
+				
+				while(true) {
+					String input = sc.nextLine();
+					
+					if(input.equalsIgnoreCase("0")) {
+						System.out.println("Thankyou");
+						break;
+					}
+					
+					w.write(input);
+					w.newLine();
+					w.flush();
+				}
+			}
+		}catch(IOException e) {
+			System.out.println(e);
+		}finally {
+			sc.close();
+		}
+		
+	}
 }
